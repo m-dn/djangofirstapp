5119e955ac95f4a3e7aaa194505835526be97ced
m-dn <mazur_damian@wp.pl>
2020-06-09 21:59:20 +0200

    Added css not working

--- StockTrading/views.py
+++ StockTrading/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Stock
+
+# def freeze(stocks):
+#     if isinstance(stocks, dict):
+#         return frozenset((key, freeze(value)) for key, value in stocks.items())
+#     elif isinstance(stocks, list):
+#         return tuple(freeze(value) for value in stocks)
+#     return stocks
 
 def stock_list(request):
-    return render(request, 'StockTrading/stock_list.html', {})
+    stocks = Stock.objects.all
+    return render(request, 'StockTrading/stock_list.html', {'stocks': stocks})
